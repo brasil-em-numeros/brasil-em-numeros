c09e97a9e77e05916e865429c847019f9080d2f5
Felipe Valladao <felipe.valladao@gmail.com>
2020-06-03 22:56:21 +0100

    heatmap

--- dashboard/provedores/pdt/assets/pdt.js
+++ dashboard/provedores/pdt/assets/pdt.js
@@ -1,7 +1,44 @@
-TESTER = document.getElementById('pdt-chart');
-Plotly.newPlot(
-    TESTER, [{
-        x: [1, 2, 3, 4, 5],
-        y: [1, 2, 4, 8, 16]
-    }], {margin: { t: 0 } }
-);
+
+// fetch("/pdt/pdt_data").then(
+//     response => response.json()
+// ).then(
+//     data => plot_pdt(data)
+// );
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, {});
+    
+//     var orgao  = document.getElementById('orgao-gov');
+//     var funcao = document.getElementById('funcao-gov');
+    
+//     orgao.addEventListener('change', update_and_load);
+//     funcao.addEventListener('change', update_and_load);
+
+// });
+
+// function update_and_load(e){
+
+//     var select = e.target
+//     select = M.FormSelect.getInstance(select)
+//     const name = select.el.id.split("-")[0]
+
+//     fetch("/pdt/pdt", {
+//         method : 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({[name] : select.getSelectedValues()})
+//     }).then(
+//         res => {
+//             fetch("/pdt/pdt_data").then(
+//                 res => res.json()
+//             ).then(
+//                 data => plot_pdt(data)
+//             )
+//         }
+//     );
+
+//     return null;
+// };
--- dashboard/provedores/pdt/despesas_publicas.py
+++ dashboard/provedores/pdt/despesas_publicas.py
@@ -1,7 +1,11 @@
 import re
+import math
 import pandas as pd
+import datetime
+import plotly.express as px
+import plotly.graph_objects as go
 
-from collections import ChainMap
+from collections import ChainMap, OrderedDict
 from .downloader import download
 
 
@@ -64,3 +68,217 @@ def despesas():
         )
     )
 
+
+date_dict = OrderedDict(
+    jan = "jan",
+    feb = "fev",
+    mar = "mar",
+    apr = "abr",
+    may = "mai",
+    jun = "jun",
+    jul = "jul",
+    aug = "ago",
+    sep = "set",
+    oct = "out",
+    nov = "nov",
+    dec = "dez",
+    January   = "Janeiro",
+    February  = "Fevereiro",
+    March     = "Março",
+    April     = "Abril",
+    May       = "Maio",
+    June      = "Junho",
+    July      = 'Julho',
+    August    = "Agosto",
+    September = "Setembro",
+    October   = "Outubro",
+    November  = "Novembro",
+    December  = "Dezembro"
+)
+
+# ----------------------------
+#  Gráficos sobre as despesas
+# ----------------------------
+
+# 1 - heatmap por mês e ano
+
+
+def heatmap(desp):
+
+    desp = desp.query(
+        "modalidade == 'Pago'"
+    ).query(
+        "valor != 0"
+    ).assign(
+        data = lambda df: pd.to_datetime(
+            df['ano_mes'] + "/01", format = "%Y/%m/%d"
+        )
+    ).assign(
+        mes = lambda df: df['data'].apply(lambda d: d.strftime("%b").lower()),
+        ano = lambda df: df['data'].dt.year
+    ).groupby(
+        ['ano', 'mes'],
+        as_index = False
+    ).sum().pivot(
+        'ano', 'mes', 'valor'
+    ).sort_index()
+
+    def key(x):
+        for i, k in enumerate(date_dict.keys()):
+            if x == k:
+                break
+        
+        return i
+
+    desp = desp[sorted(desp.columns, key = key)]
+    fig  = px.imshow(
+        desp.to_numpy(),
+        x = [date_dict.get(c, c).title() for c in desp.columns],
+        y = [str(i) for i in desp.index],
+        labels = dict(color = "Gastos pagos"),
+        color_continuous_scale = "RdYlGn_r"
+    )
+
+    return fig
+
+
+def funcao_por_ano(despesas):
+
+    df = despesas.query(
+        "modalidade == 'Pago'"
+    ).groupby(
+        ['ano_mes', "Função"], as_index = False
+    ).sum()
+
+    df['data'] = df['ano_mes'] + "/01"
+    df['data'] = pd.to_datetime(df['data'], format = '%Y/%m/%d')
+
+    fig = go.Figure()
+    hover_template = "<b>%{text}</b><br>Gasto: %{y:$,.0f}"
+    funs = sorted(df['Função'].unique())
+    for fun in funs:
+        
+        plot_data = df.loc[df['Função'] == fun]
+        x = plot_data['data']
+        y = plot_data['valor']
+        fig.add_trace(
+            go.Scatter(
+                x = x,
+                y = y,
+                mode = "lines+markers",
+                name = fun,
+                text = plot_data['Função'],
+                line = {'shape' : 'spline'},
+                hovertemplate = hover_template
+            )
+        )
+
+    fig.update_layout(yaxis_title = "Valor Pago")
+    fig.update_layout(title = "Gastos por categoria")
+    return fig
+
+
+def gastos_por_ministerio(despesas):
+
+    df = despesas.groupby(
+        ['ano_mes', 'Órgão Superior'], as_index = False
+    ).sum()
+
+    fig  = go.Figure()
+
+    # ----------------------
+    #  Top 10 ministérios
+    # ----------------------
+
+    top10 = df.groupby(
+        ['ano_mes'],
+        as_index = False
+    ).rank(method = "min", ascending = False)
+
+    top10.columns = ['rank']
+    df = pd.merge(
+        df, top10,
+        how = "left",
+        left_index = True,
+        right_index = True
+    ).assign(
+        ministerio = lambda x: [
+            "Outros" if r > 10 else m for m, r in zip(
+                x['Órgão Superior'],
+                x['rank']
+            )
+        ]
+    ).groupby(
+        ['ano_mes', 'ministerio'],
+        as_index = False
+    ).sum().drop(
+        columns = ['rank']
+    ).assign(
+        data = lambda x: x['ano_mes'].apply(
+            lambda d: datetime.datetime.strptime(d + "/01", "%Y/%m/%d").strftime("%b/%Y")
+        )
+    )
+
+    # --------------------------
+    #  Calcula tamanho do eixo
+    # --------------------------
+
+    x_max = max(df['valor'])
+    zeros = int(math.log10(x_max))
+    x_final = x_max / pow(10, zeros)
+    if (x_final % 1) < 0.5:
+        x_final = int(x_final) + 0.5
+    else:
+        x_final = int(x_final) + 1.0
+    eixo_x = [0, x_final * pow(10, zeros)]
+
+    # ----------------------
+    #  Calcula cada quadro
+    # ----------------------
+
+    for dt, grp in df.groupby(['data']):
+        fig.add_trace(
+            go.Scatter(
+                visible=False,
+                name = dt,
+                x = grp['valor'],
+                y = grp['ministerio'],
+                mode = "markers"
+            )
+        )
+
+    fig.data[0].visible = True
+
+    # Create and add slider
+    steps = []
+    for i in range(len(fig.data)):
+        step = dict(
+            method = "update",
+            args = [
+                {"visible": [False] * len(fig.data)}
+            ],  # layout attribute
+        )
+        step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+        steps.append(step)
+
+    sliders = [
+        dict(
+            active = 0,
+            currentvalue = {"prefix": "Data: "},
+            pad = {"t": 50},
+            steps = steps
+        )
+    ]
+
+    fig.update_layout(
+        sliders = sliders,
+        title = "Top 10 ministérios com maior gasto no mês"
+        # -- Usar o abaixo com dados reais
+        # xaxis = dict(range = eixo_x, autorange = False)
+    )
+
+    fig.update_xaxes( # the y-axis is in dollars
+        showgrid=False
+    )
+
+    return fig
