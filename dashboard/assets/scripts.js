95ab409bed25c2b05eeae37a744347e8d85f3a53
Wagner Leonardi <leonardiwagner@gmail.com>
2020-06-03 12:42:11 -0300

    remoção do jquery

--- dashboard/assets/scripts.js
+++ dashboard/assets/scripts.js
@@ -1,88 +1,27 @@
-/*!
-    * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict";
-
-    // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-            if (this.href === path) {
-                $(this).addClass("active");
-            }
-        });
-
-    // Toggle the side navigation
-    $("#sidebarToggle").on("click", function(e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-})(jQuery);
-
-
-function plot_pdt(data){
-    return Plotly.newPlot(
-        TESTER, data,
-        {margin: { t: 0 } }
-    );
-};
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    // var elems = document.getElementById('pdt-date');
-    // var instances = M.Datepicker.init(elems, {});
-    // elems.M_Datepicker.options.i18n.months = [
-    //     'Janeiro', 'Fevereiro', 'Março',
-    //     'Abril', 'Maio', 'Junho', 'Julho',
-    //     'Agosto', 'Setembro', 'Outubro',
-    //     'Novembro', 'Dezembro'
-    // ];
-
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, {});
-
-    fetch("/pdt/pdt_data").then(
-        response => response.json()
-    ).then(
-        data => plot_pdt(data)
-    )
-
-  });
-
-// slider = document.getElementById("pdt-slider");
-// slider.addEventListener("input", e => {
-//     fetch("/pdt/pdt", {
-//         method : 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(slider.value)
-//     }).then(
-//         response => {
-//             if(response.status == 200){
-//                 return response.ok
-//             } 
-
-//             throw 'POST request to pdt data did not succeed.'
-//         }
-//     ).then(
-//         res => {
-//             fetch("/pdt/pdt_data").then(
-//                 response => response.json()
-//             ).then(
-//                 data => plot_pdt(data)
-//             )
-//         }
-//     )
-// });
-
-TESTER = document.getElementById('pdt-chart')
-TESTER.addEventListener('DOMContentLoaded', function(){
-    fetch("/pdt/pdt_data").then(
-        response => response.json()
-    ).then(
-        data => plot_pdt(data)
-    )
+document.querySelectorAll("#layoutSidenav_nav .sb-sidenav a.nav-link").forEach(function toggleNavlinks(e){
+    if (e.href === window.location.href) {
+        e.classList.add("active");
+    }
 })
+
+document.getElementById("sidebarToggle").addEventListener("click", function toggleSidebar(){
+    const body = document.querySelector("body")
+    const style = "sb-sidenav-toggled"
+
+    body.classList.contains(style) ?
+        body.classList.remove(style) :
+        body.classList.add(style)
+})
+
+const fetchData = (url, callback) =>
+  fetch(url)
+    .then(response => response.json())
+    .then(callback)
+
+const graphicElement = document.getElementById('pdt-chart')
+const plot_pdt = (data) => Plotly.newPlot(
+    graphicElement, data,
+    {margin: { t: 0 } }
+);
+
+graphicElement && fetchData("/pdt/pdt_data", plot_pdt)
